afff63315923804181202680434558425696f68f
fae-coder <rxd511@york.ac.uk>
2025-10-23 16:51:16 +0100

    .

--- Assets/opendoor.cs
+++ Assets/opendoor.cs
@@ -4,9 +4,13 @@ public class opendoor : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
-    }
+    void OnTriggerEnter (Collider other)
+       {
+     GameObject parent = transform.parent.gameObject;       
+     Animation animation = parent.GetComponent<Animation>();       
+     animation.Play("OpenDoor");
+       }
+
 
     // Update is called once per frame
     void Update()
@@ -14,3 +18,5 @@ public class opendoor : MonoBehaviour
         
     }
 }
+
+  
